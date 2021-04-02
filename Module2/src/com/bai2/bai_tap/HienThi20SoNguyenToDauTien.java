b05d32def1eb08c908aa9b30147dacbd2d4caf5d
C0221G1-Nguyễn Công Tín <nguyencongtin1551997@gmail.com>
2021-04-02 08:16:48 +0700

    upbai

---
+++ Module2/src/com/bai2/bai_tap/HienThi20SoNguyenToDauTien.java
@@ -0,0 +1,56 @@
+package com.bai2.bai_tap;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class HienThi20SoNguyenToDauTien {
+    //    public static void main(String[] args) {
+//         int num=0;
+//         StringBuilder result= new StringBuilder(0);
+//         int count=0;
+//        while (count<20) {
+//             boolean check = true;
+//            if (num < 2) {
+//                check = false;
+//            } else {
+//                for ( int i = 2; i <= Math.sqrt(num); i++) {
+//                    if (num % i == 0) {
+//                        check = false;
+//                        break;
+//                    }
+//                }
+//            }
+//            if (check) {
+//                count++;
+//                result.append(num).append(" ");
+//            }
+//            num++;
+//        }
+//        System.out.println(result);
+//    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        int count = 0;
+        while (count < 20) {
+            boolean check = true;
+            if (num < 2) {
+                check = false;
+            } else {
+                for (int i = 2; i <= Math.sqrt(num); i++) {
+                    if (num % i == 0) {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check) {
+                    System.out.print(" "+num);
+                    count++;
+                }
+            }
+            num++;
+        }
+    }
+}
