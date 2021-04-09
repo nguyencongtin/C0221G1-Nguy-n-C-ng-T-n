e708ee69297e5c8fd955413293c704d841107f8c
C0221G1-Nguyễn Công Tín <nguyencongtin1551997@gmail.com>
2021-04-09 11:31:39 +0700

    upbai

---
+++ Module2/src/com/bai8/bai_tap/TennisGame.java
@@ -0,0 +1,75 @@
+package com.bai8.bai_tap;
+
+public class TennisGame {
+    public static String getScore(String player1Name, String player2Name, int scorePlayer1, int scorePlayer2) {
+        String score = "";
+        int tempScore = 0;
+        if (scorePlayer1 == scorePlayer2) {
+            score = getScoreRound(scorePlayer1);
+        } else if (scorePlayer1 >= 4 || scorePlayer2 >= 4) {
+            score = getPlayerWinner(scorePlayer1, scorePlayer2);
+        } else {
+            score = getScore(scorePlayer1, scorePlayer2, score);
+        }
+        return score;
+    }
+
+    private static String getScoreRound(int scorePlayer1) {
+        String score;
+        switch (scorePlayer1) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
+        }
+        return score;
+    }
+
+    private static String getPlayerWinner(int scorePlayer1, int scorePlayer2) {
+        String score;
+        int minusResult = scorePlayer1 - scorePlayer2;
+        if (minusResult == 1) score = "Advantage player1";
+        else if (minusResult == -1) score = "Advantage player2";
+        else if (minusResult >= 2) score = "Win for player1";
+        else score = "Win for player2";
+        return score;
+    }
+
+    private static String getScore(int m_score1, int m_score2, String score) {
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = m_score1;
+            else {
+                score += "-";
+                tempScore = m_score2;
+            }
+            switch (tempScore) {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
+            }
+        }
+        return score;
+    }
+}
