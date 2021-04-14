2779acf8457ad471028dee5a20f572acb7226860
C0221G1-Nguyễn Công Tín <nguyencongtin1551997@gmail.com>
2021-04-14 08:15:57 +0700

    upbai

---
+++ Module2/src/com/bai11/bai_tap/daoNguocPhanTuTrongMangSoNguyenSuDungStack/DaoNguocPhanTu.java
@@ -0,0 +1,24 @@
+package com.bai11.bai_tap.daoNguocPhanTuTrongMangSoNguyenSuDungStack;
+
+import java.util.ArrayList;
+import java.util.Stack;
+public class DaoNguocPhanTu {
+    private static void DaoNguocMang(ArrayList<Integer> list) {
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = list.size(); i >0 ; i--) {
+            stack.push(list.remove(0));
+        }
+        for (int i =stack.size(); i >0 ; i--) {
+            list.add(stack.pop());
+        }
+        System.out.println("Sau " + list);
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        System.out.println("Trước " + list);
+    }
+}
