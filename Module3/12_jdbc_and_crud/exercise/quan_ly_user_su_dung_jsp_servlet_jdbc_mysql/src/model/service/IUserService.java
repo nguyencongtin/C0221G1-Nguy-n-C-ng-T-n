7fea7650cb8b4e7817e99f618cf6ca71278f14a6
C0221G1-Nguyễn Công Tín <nguyencongtin1551997@gmail.com>
2021-05-31 19:37:03 +0700

    update

---
+++ Module3/12_jdbc_and_crud/exercise/quan_ly_user_su_dung_jsp_servlet_jdbc_mysql/src/model/service/IUserService.java
@@ -0,0 +1,15 @@
+package model.service;
+
+import model.bean.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUserService {
+    List<User> findByAll();
+    User findById(int id);
+    boolean update(int id, User user);
+    boolean add(User user);
+    boolean remove(int id);
+    List<User> findByCountry(String country1);
+}
