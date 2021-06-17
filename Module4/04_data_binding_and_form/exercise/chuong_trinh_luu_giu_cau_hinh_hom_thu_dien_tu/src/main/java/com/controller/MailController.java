df55499e63410c1bbaacfa62ec472435b5acfd6f
C0221G1-Nguyễn Công Tín <nguyencongtin1551997@gmail.com>
2021-06-17 15:17:44 +0700

    up bai tap ngay 17/6

---
+++ Module4/04_data_binding_and_form/exercise/chuong_trinh_luu_giu_cau_hinh_hom_thu_dien_tu/src/main/java/com/controller/MailController.java
@@ -0,0 +1,24 @@
+package com.controller;
+
+import com.model.Mail;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MailController {
+    @GetMapping(value = {"","/mail"})
+    public String goFormMail(Model model){
+        model.addAttribute("mail",new Mail());
+        return "home";
+    }
+
+    @PostMapping(value = "/update")
+    public ModelAndView update(@ModelAttribute Mail mail){
+        return  new ModelAndView("show","mail",mail);
+    }
+}
---
+++ Module4/04_data_binding_and_form/exercise/chuong_trinh_luu_giu_cau_hinh_hom_thu_dien_tu/src/main/webapp/WEB-INF/views/show.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+Language : ${mail.language}
+Size : ${mail.size}
+Spam : ${mail.spam}
+Signature : ${mail.signature}
+</body>
+</html>
