7e50ec23a3ad91ac8b5661a00cd2ec9297b5a58a
C0221G1-Nguyễn Công Tín <nguyencongtin1551997@gmail.com>
2021-06-28 20:06:28 +0700

    up bai tap ngay 28/6

---
+++ Module4/09_aop/practice/su_dung_exception_handler_de_xu_ly_ngoai_le_cho_ung_dung_quan_ly_khach_hang/src/main/java/com/example/su_dung_exception_handler_de_xu_ly_ngoai_le_cho_ung_dung_quan_ly_khach_hang/model/Province.java
@@ -0,0 +1,4 @@
+package com.example.su_dung_exception_handler_de_xu_ly_ngoai_le_cho_ung_dung_quan_ly_khach_hang.model;
+
+public class Province {
+}
---
+++ Module4/bo_sung_aspect_log_cho_ung_dung_quan_ly_danh_sach_khach_hang/src/main/java/com/example/bo_sung_aspect_log_cho_ung_dung_quan_ly_danh_sach_khach_hang/controller/CustomerController.java
@@ -0,0 +1,4 @@
+package com.example.bo_sung_aspect_log_cho_ung_dung_quan_ly_danh_sach_khach_hang.controller;
+
+public class CustomerController {
+}
