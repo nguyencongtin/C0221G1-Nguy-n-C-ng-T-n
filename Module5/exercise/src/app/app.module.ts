d569e191f066166a475fdea87cb4f058ff6aa12b
C0221G1-Nguyễn Công Tín <nguyencongtin1551997@gmail.com>
2021-07-22 19:57:30 +0700

    up bai tap ngay 22/7

--- Module5/exercise/src/app/app.module.ts
+++ Module5/exercise/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './03_angular_overview/calculator/calculator.component';
-import { TypescriptVariableComponent } from './02_typescript/typescript-variable/typescript-variable.component';
+import { ColorComponent } from './03_angular_overview/color/color.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    TypescriptVariableComponent
+    ColorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
